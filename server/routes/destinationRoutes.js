13a46d4af454a6710be7f3cbe492e838e2cd5ff6
thivyan <166355865+thivyan-jeyamohan@users.noreply.github.com>
2025-04-28 20:48:27 +0530

    Delete server directory

--- server/routes/destinationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getDestinationWithWeather } from "../controllers/destinationController.js";
-
-const router = express.Router();
-
-// Route to get a destination with weather details
-router.get("/destination/:id", getDestinationWithWeather);  // Fetch destination with weather data
-
-export default router;
--- server/routes/weatherRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getWeather } from '../controllers/weatherController.js';  // Ensure the path is correct
-
-const router = express.Router();
-
-// Weather API route
-router.post('/', getWeather);
-
-export default router;  // Export the router
